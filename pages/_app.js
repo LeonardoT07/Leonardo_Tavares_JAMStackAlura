c0b6f29c83f9661c8565e596a95aa39ce653c1cd
Leonardo Tavares <leonardotg07@hotmail.com>
2021-05-23 21:01:15 -0300

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,13 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../src/theme/GlobalStyle';
+import { theme } from '../src/theme';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  text-align: center;
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>Reformulação de Layout</Title>
 }
