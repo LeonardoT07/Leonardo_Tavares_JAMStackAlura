14c704ae1ce930727ffa5ca8d88c4dbc4ca77600
Leonardo Tavares <leonardotg07@hotmail.com>
2021-05-26 20:55:43 -0300

    implementação base para inicio de projeto

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '../src/theme/GlobalStyle';
-import { theme } from '../src/theme';
-
+import GlobalStyle from '../src/theme/GlobalStyle';
+import theme from '../src/theme/index';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,41 @@
-export const theme = {
-    colors: {
-      primary: '#0070f3',
+export const colors = {
+  dark: {
+    primary: '#070C0E',
+    secondary: '',
+    tertiary: '',
+
+    background: {
+      primary: '#070C0E',
+      secondary: '',
+      tertiary: '',
     },
-  }
+  },
+
+  light: {
+    primary: '#070C0E',
+    secondary: '',
+    tertiary: '',
+
+    background: {
+      primary: '#070C0E',
+      secondary: '',
+      tertiary: '',
+    },
+  },
+};
+
+export const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export default {
+  colors,
+  breakpoints,
+  fontFamily: '\'Rubik\', sans-serif',
+  borderRadius: '12px',
+  transition: '200ms ease-in-out',
+};
